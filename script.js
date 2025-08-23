4f3260e915edb8bf87431d956cbdde82ba3e860f
Abu Bakar Siddique <siddique0203@gmail.com>
2025-08-23 11:00:08 +0600

    img

--- script.js
+++ script.js
@@ -44,11 +44,11 @@ accordions.forEach(btn => {
 let currentProject = null;  
 function showProject(project) {
     const details = {
-        govtaid: "<h3>GovtAid</h3><p>A C# project working like a freelancing platform where users can access government-related services easily with broker assistance.</p>",
+        govtaid: "<h3>GovtAid</h3><p>A C# project working like a freelancing platform where users can access government-related services easily with broker assistance.</p><img src='image/7.png' style=' margin-left:5%;  vertical-align:top; '>",
         sms: "<h3>Student Management System</h3><p>A Java project for managing students of American International School.</p>",
-        fixtureflow: "<h3>FixtureFlow</h3><p>A web-based tournament fixture generator with bye calculation and updated tables.</p>",
-        robosoccer: "<h3>Robo Soccer</h3><p>Participated in JUSC National Science Festival 2024 and East West University Robo Fest with a soccer bot.</p>",
-        footballbot: "<h3>Football Carrying Bot</h3><p>An RC car designed to carry footballs during matches.</p>",
+        fixtureflow: "<h3>FixtureFlow</h3><p>A web-based tournament fixture generator with bye calculation and updated tables.</p><img src='image/5.png' style='width:700px;  vertical-align:top; '><img src='image/6.png' style='width:700px;  vertical-align:top;margin-left:30px;'>",
+        robosoccer: "<h3>Robo Soccer</h3><p>Participated in JUSC National Science Festival 2024 and East West University Robo Fest with a soccer bot.</p><img src='image/1.jpg' style='width:700px;  vertical-align:top; '><img src='image/3.jpg' style='width:700px;  vertical-align:top;margin-left:30px;'>",
+        footballbot: "<h3>Football Carrying Bot</h3><p>An RC car designed to carry footballs during matches.</p><img src='image/2.jpg' style='width:700px;  vertical-align:top;margin-left:30px;'><img src='image/4.jpg' style='width:700px;  vertical-align:top;margin-left:30px;'>",
         thiefdetector: "<h3>Thief Detector</h3><p>Arduino-based security system that detects intrusion and triggers alarm.</p>",
         waterlevel: "<h3>Water Level Indicator</h3><p>Created with transistor, buzzer, resistor, and LED to indicate tank water levels.</p>",
         agriconnect: "<h3>AgriConnect</h3><p>A software engineering course project connecting farmers with micro-entrepreneurs and consumers.</p>"
